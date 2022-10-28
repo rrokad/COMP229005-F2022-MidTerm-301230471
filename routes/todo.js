fd46abbdefd1f89da90e9342989013cd49cd5650
rrokad <107492186+rrokad@users.noreply.github.com>
2022-10-28 06:39:01 -0400

    Part 3

--- routes/todo.js
+++ routes/todo.js
@@ -1,3 +1,6 @@
+/* Student Name : Rushi Rokad
+   StudentID : 301230471      */ 
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,7 +10,13 @@ let todoController = require('../controllers/todo');
 function requireAuth(req, res, next)
 {
     // check if the user is logged in
-    
+    if (!req.isAuthenticated()) {
+        req.session.url = req.originalUrl;
+        return res.redirect('/users/signin');
+    } else {
+        next();
+    }
+
     // ADD YOUR CODE HERE        
 
 }
@@ -19,16 +28,17 @@ router.get('/list', todoController.todoList);
 router.get('/details/:id', todoController.details);
 
 // Routers for edit
-router.get('/edit/:id', todoController.displayEditPage);
-router.post('/edit/:id', todoController.processEditPage);
+router.get('/edit/:id', requireAuth, todoController.displayEditPage);
+router.post('/edit/:id', requireAuth, todoController.processEditPage);
 
 // Delete
-router.get('/delete/:id', todoController.performDelete);
+router.get('/delete/:id', requireAuth, todoController.performDelete);
 
 /* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', todoController.displayAddPage);
+router.get('/add', requireAuth,  todoController.displayAddPage);
+
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', todoController.processAddPage);
+router.post('/add', requireAuth, todoController.processAddPage);
 
 module.exports = router;
